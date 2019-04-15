71deee5feae66db6d86382ac926fbaf8ad332e8e
Lucas Pacheco <lucaspsilveira@live.com>
2019-04-14 21:14:36 -0300

    Remoção de importações não utilizadas

--- ClienteServlet.java
+++ ClienteServlet.java
@@ -7,8 +7,6 @@ package controller;
 
 import dao.ClienteDAO;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.List;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
